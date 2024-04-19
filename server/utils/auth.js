cfd26d80a32c392012607d16bc618bd87b59745e
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.attlocal.net>
2024-04-19 12:39:14 -0400

    updated auth.js to work with GraphQL

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,10 +7,16 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  // Gives an error message if the user is not authenticated
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
