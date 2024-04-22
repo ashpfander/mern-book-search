b2070b3088cf051d943bf14e711ea6b3c9cd0251
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-04-22 15:37:55 -0400

    added queries to front-end and created mutations file

---
+++ client/src/utils/queries.js
@@ -0,0 +1,58 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USERS = gql`
+  query users {
+    user {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const QUERY_SINGLE_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
